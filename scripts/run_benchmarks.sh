d591daf18e50efbd74edd69c93a8c0fc67104ed8
Samarium <28302241+Samarium150@users.noreply.github.com>
2022-12-01 10:58:42 -0700

    fix: remove repetitions

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -9,13 +9,11 @@ cd ../highway
 bazel build -c opt //hwy/contrib/sort:all
 echo "Run benchmarks..."
 echo "Run sequential sorting..."
-bazel-bin/hwy/contrib/sort/bench_sort >> bench_sort.txt
+bazel-bin/hwy/contrib/sort/bench_sort > bench_sort.txt
 for ((i=0; i<7; i++)); do
     echo "Run parallel sorting round ${i}..."
-    bazel-bin/hwy/contrib/sort/bench_parallel >> bench_parallel_${i}.txt
+    bazel-bin/hwy/contrib/sort/bench_parallel > bench_parallel_${i}.txt
 done
-bazel-bin/hwy/contrib/sort/bench_sort > bench_sort.txt
-bazel-bin/hwy/contrib/sort/bench_parallel > bench_parallel.txt
 mv bench_sort.txt ../scripts/results/
 mv bench_parallel*.txt ../scripts/results/parallel/
 echo "Reset submodules..."
